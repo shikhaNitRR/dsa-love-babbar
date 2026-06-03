3ebdf5fe4a8591d0225a80e0d54ce084d789d97a
Shikha <shikha.sri96@gmail.com>
2026-06-03 18:34:43 +0530

    NumericPattern

--- src/javaDsa/allPatterns/NumericPatterns.java
+++ src/javaDsa/allPatterns/NumericPatterns.java
@@ -3,13 +3,28 @@ package javaDsa.allPatterns;
 public class NumericPatterns {
     public static void main(String[] args) {
 
-        for(int i=1;i<=5;i++) {
+        for (int i = 1; i <= 5; i++) {
 
-            for(int j=1;j<=i;j++) {
+            for (int j = 1; j <= i; j++) {
 
-                System.out.print(j+" ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
+
+        System.out.println();
+//pattern2
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+
     }
 }
