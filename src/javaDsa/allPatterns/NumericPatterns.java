f040b89047f24756c0a1bde454ad755a33ed3910
Shikha <shikha.sri96@gmail.com>
2026-06-04 10:37:28 +0530

    new NumericPattern

--- src/javaDsa/allPatterns/NumericPatterns.java
+++ src/javaDsa/allPatterns/NumericPatterns.java
@@ -36,6 +36,17 @@ k++;
             System.out.println();
 
         }
+//Pattern4
+        System.out.println();
+        int count=0;
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(++count + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
