715632a19584da63926396cf3d71da24527394bb
Shikha <shikha.sri96@gmail.com>
2026-06-08 17:58:16 +0530

    AlphabeticPattern

---
+++ src/javaDsa/allPatterns/AlphabeticPattern.java
@@ -0,0 +1,30 @@
+package javaDsa.allPatterns;
+
+public class AlphabeticPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                int h = 'A' - 1;
+                System.out.print((char) (h + j) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //OR
+
+        int a=65;
+        for(int i=65;i<=70;i++) {
+
+            for(int j=a;j<=i;j++) {
+
+                System.out.print((char)(j)+" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+}
