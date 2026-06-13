037ed9a170c8277f9d550936a8395073d070f714
Shikha <shikha.sri96@gmail.com>
2026-06-14 00:05:48 +0530

    String basics

--- src/javaDsa/strings/BasicString.java
+++ src/javaDsa/strings/BasicString.java
@@ -1,4 +1,27 @@
 package javaDsa.strings;
 
 public class BasicString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+        System.out.println("Length of string : " + count);
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+        System.out.println("Reversed string : " + reversed);
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome+ ");
+        } else {
+            System.out.println("Not a Palindrome ");
+        }
+    }
 }
