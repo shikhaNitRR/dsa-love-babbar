f1f00d4ea552ee869206239f4b55324b5ce09b77
Shikha <shikha.sri96@gmail.com>
2026-06-16 01:16:04 +0530

    formatting

--- src/javaDsa/allPatterns/Patterns.java
+++ src/javaDsa/allPatterns/Patterns.java
@@ -5,16 +5,16 @@ public class Patterns {
     public static void main(String[] args) {
         //Pattern1
         System.out.println("======Pattern1=====Solid Rectangle ");
-        for(int i=1;i<=3;i++){
-            for(int j=1;j<=5;j++){
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         //Pattern2
         System.out.println("======Pattern2=====Right Angled Triangle ....left sided===============");
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
@@ -33,11 +33,11 @@ public class Patterns {
         }
 
         //Pattern4
-        int n=5;
+        int n = 5;
         System.out.println("======Pattern4=====Inverted Right angled triangle");
-        for(int i=1;i<=n;i++){
+        for (int i = 1; i <= n; i++) {
             //stars
-            for(int j=1;j<=n-i+1;j++){
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
             }
             //move to next line
@@ -47,13 +47,13 @@ public class Patterns {
         //Pattern5
         System.out.println("======Pattern5=====Solid Rhombus ");
 
-        for(int i=1;i<=n;i++){
+        for (int i = 1; i <= n; i++) {
             //spaces
-            for(int j=1;j<=n-i;j++){
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print("  ");
             }
             //star
-            for(int j=1;j<=n;j++){
+            for (int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
             //move to next line
