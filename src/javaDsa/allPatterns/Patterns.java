598ec14295ada4ef475302a53b47ce354e31ad19
Shikha <shikha.sri96@gmail.com>
2026-06-16 00:59:39 +0530

    right Angled Triangle right-sided

--- src/javaDsa/allPatterns/Patterns.java
+++ src/javaDsa/allPatterns/Patterns.java
@@ -12,7 +12,7 @@ public class Patterns {
             System.out.println();
         }
         //Pattern2
-        System.out.println("======Pattern2=====Right Angled Triangle ");
+        System.out.println("======Pattern2=====Right Angled Triangle ....left sided===============");
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
@@ -20,8 +20,33 @@ public class Patterns {
             System.out.println(" ");
         }
         //Pattern3
-        System.out.println("======Pattern3=====Solid Rhombus ");
+        System.out.println("======Pattern3=====Right Angled Triangle ....right sided==============");
+        int l = 5;
+        for (int i = 1; i <= l; i++) {
+            for (int j = 1; j <= l - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //Pattern4
         int n=5;
+        System.out.println("======Pattern4=====Inverted Right angled triangle");
+        for(int i=1;i<=n;i++){
+            //stars
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+            //move to next line
+            System.out.println();
+        }
+
+        //Pattern5
+        System.out.println("======Pattern5=====Solid Rhombus ");
+
         for(int i=1;i<=n;i++){
             //spaces
             for(int j=1;j<=n-i;j++){
@@ -29,16 +54,6 @@ public class Patterns {
             }
             //star
             for(int j=1;j<=n;j++){
-                System.out.print("* ");
-            }
-            //move to next line
-            System.out.println();
-        }
-
-        System.out.println("======Pattern4=====Inverted Right angled triangle");
-        for(int i=1;i<=n;i++){
-            //stars
-            for(int j=1;j<=n-i+1;j++){
                 System.out.print("* ");
             }
             //move to next line
